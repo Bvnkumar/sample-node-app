f34df0af8225e2e92128b8074bf5602c26d9ec75
Bvnkumar <bvnkumar007@gmail.com>
2022-05-09 22:21:33 -0400

    Fix lint errors

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,48 @@
-var express = require("express");
-var router = express.Router();
-var indexServices = require("../services/index");
+var express = require('express')
+var router = express.Router()
+var indexServices = require('../services/index')
 /* GET home page. */
-router.get("/", async function (req, res, next) {
-  res.render("index", { title: "Express123" });
-});
+router.get('/', async function (req, res, next) {
+  res.render('index', { title: 'Express123' })
+})
 
-router.get("/getusers", function (req, res) {
+router.get('/getusers', function (req, res) {
   indexServices.getUsers((err, result) => {
-    if (err) res.status(400).send(err);
-    res.status(200).send(result);
-  });
-});
-router.post("/post/kafka/message", async function (req, res) {
-  await producer.connect();
+    if (err) res.status(400).send(err)
+    res.status(200).send(result)
+  })
+})
+router.post('/post/kafka/message', async function (req, res) {
+  await producer.connect()
   payloads = {
-    topic: "test",
-    messages: [{ key: "coronavirus-alert", value: "test message" }],
-  };
-  producer.send(payloads);
-  res.status(200).send("A message has been inserted");
-});
+    topic: 'test',
+    messages: [{ key: 'coronavirus-alert', value: 'test message' }]
+  }
+  producer.send(payloads)
+  res.status(200).send('A message has been inserted')
+})
 
 /**
  * @param no inputs required
  * @returns file data in utf8 format
  */
-router.get("/fileread", function (req, res) {
-  pathname = path.join(__dirname, "../public/files/sample.txt");
-  console.log("pathname ", pathname);
-  fs.appendFile(pathname, " How are you?", "utf8", (err, writeResult) => {
+router.get('/fileread', function (req, res) {
+  pathname = path.join(__dirname, '../public/files/sample.txt')
+  console.log('pathname ', pathname)
+  fs.appendFile(pathname, ' How are you?', 'utf8', (err, writeResult) => {
     if (err) {
     } else {
-      log.info("File writing is completed " + writeResult);
-      fs.readFile(pathname, "utf8", function (err, result) {
+      log.info('File writing is completed ' + writeResult)
+      fs.readFile(pathname, 'utf8', function (err, result) {
         if (err) {
-          console.log("err ", err);
+          console.log('err ', err)
         } else {
-          log.info("File reading  is completed " + result);
-          console.log("result ", result);
+          log.info('File reading  is completed ' + result)
+          console.log('result ', result)
         }
-        res.send(result);
-      });
+        res.send(result)
+      })
     }
-  });
-});
-module.exports = router;
+  })
+})
+module.exports = router
